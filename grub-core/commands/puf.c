17a973d1225d9d4a4f133a4c3c6de531a8257433
drazere <oldisgoldgta@gmail.com>
2026-05-16 23:52:07 +0530

    Added basic linux mailbox

--- grub-core/commands/puf.c
+++ grub-core/commands/puf.c
@@ -5,18 +5,37 @@
 #include <grub/misc.h>
 #include <grub/mm.h>
 #include <grub/term.h>
-
-#include <grub/efi/efi.h>
-#include <grub/efi/api.h>
+#include <grub/types.h>
 
 GRUB_MOD_LICENSE("GPLv3");
 
-static grub_guid_t cortex_guid =
+/*
+ * IMPORTANT:
+ *
+ * Add these kernel parameters:
+ *
+ * memmap=4M!4G nokaslr
+ *
+ * Example GRUB linux line:
+ *
+ * linux /boot/vmlinuz-linux root=/dev/nvme0n1p4 rw memmap=4M!4G nokaslr
+ *
+ * This reserves a persistent memory region starting at:
+ *
+ * 0x100000000 (4GB)
+ */
+
+#define CORTEX_MAILBOX_ADDR 0x100000000ULL
+#define CORTEX_MAGIC        0x43525458 /* "CRTX" */
+
+struct cortex_mailbox
 {
-    0x12345678,
-    0x1234,
-    0x5678,
-    { 0xaa, 0xbb, 0xcc, 0xdd, 0xee, 0xff, 0x11, 0x22 }
+    grub_uint32_t magic;
+    grub_uint32_t version;
+
+    grub_uint32_t data_size;
+
+    grub_uint8_t puf_data[64];
 };
 
 static grub_err_t
@@ -24,29 +43,55 @@ grub_puf_dump(grub_command_t cmd __attribute__((unused)),
               int argc __attribute__((unused)),
               char **argv __attribute__((unused)))
 {
-    grub_uint8_t *addr = (grub_uint8_t *) 0x100000;
-    grub_uint8_t sample[64];
+    /*
+     * Example DRAM sampling location
+     */
+    grub_uint8_t *dram_addr =
+        (grub_uint8_t *)0x100000;
+
+    /*
+     * Mailbox pointer inside reserved persistent RAM
+     */
+    struct cortex_mailbox *mailbox =
+        (struct cortex_mailbox *)(grub_addr_t)
+            CORTEX_MAILBOX_ADDR;
+
     int i;
 
+    grub_printf("Collecting DRAM PUF sample...\n");
+
+    /*
+     * Clear mailbox
+     */
+    grub_memset(mailbox, 0, sizeof(*mailbox));
+
+    /*
+     * Fill metadata
+     */
+    mailbox->magic = CORTEX_MAGIC;
+    mailbox->version = 1;
+    mailbox->data_size = 64;
+
+    /*
+     * Copy DRAM bytes
+     */
     for (i = 0; i < 64; i++)
-        sample[i] = addr[i];
+        mailbox->puf_data[i] = dram_addr[i];
 
-    grub_printf("Writing PUF sample to EFI variable...\n");
+    grub_printf("Cortex mailbox written.\n");
 
-    grub_efi_status_t status = grub_efi_set_variable(
-        "CORTEX_PUF",
-        &cortex_guid,
-        sample,
-        sizeof(sample)
-    );
+    grub_printf("Mailbox physical address: 0x%llx\n",
+                (unsigned long long)
+                CORTEX_MAILBOX_ADDR);
 
-    if (status != GRUB_EFI_SUCCESS)
-    {
-        grub_printf("EFI write failed: %lx\n", status);
-        return GRUB_ERR_IO;
-    }
+    grub_printf("First 16 bytes:\n");
 
-    grub_printf("EFI variable written successfully.\n");
+    for (i = 0; i < 16; i++)
+        grub_printf("%02x",
+                    mailbox->puf_data[i]);
+
+    grub_printf("\n");
+
     return GRUB_ERR_NONE;
 }
 
@@ -58,7 +103,7 @@ GRUB_MOD_INIT(puf)
         "puf_dump",
         grub_puf_dump,
         0,
-        "Dump DRAM PUF to EFI variable");
+        "Dump DRAM PUF into Cortex reserved RAM mailbox");
 }
 
 GRUB_MOD_FINI(puf)
